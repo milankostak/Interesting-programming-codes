636d75bdae09ba82b796e7c5811efb1ecc53ddef
Milan Košťák <milankostak@seznam.cz>
2019-12-09 15:05:18 +0100

    SynchronizedTest

---
+++ src/SynchronizedTest.java
@@ -0,0 +1,37 @@
+public class SynchronizedTest {
+
+    private synchronized void test(String name) { // delete synchronized
+        for (int i = 0; i < 10; i++) {
+            System.out.println(name + " :: " + i);
+            try {
+                Thread.sleep(500);
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        SynchronizedTest theDemo = new SynchronizedTest();
+        new TestThread("THREAD 1", theDemo);
+        new TestThread("THREAD 2", theDemo);
+        new TestThread("THREAD 3", theDemo);
+    }
+
+    private static class TestThread extends Thread {
+        private String name;
+        private SynchronizedTest theDemo;
+
+        private TestThread(String name, SynchronizedTest theDemo) {
+            this.theDemo = theDemo;
+            this.name = name;
+            start();
+        }
+
+        @Override
+        public void run() {
+            theDemo.test(name);
+        }
+    }
+
+}
