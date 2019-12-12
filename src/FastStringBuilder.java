6283ed379603a818a2b586d29fad725cfe9093f7
Milan Košťák <milankostak@seznam.cz>
2019-12-12 21:36:58 +0100

    FastStringBuilder - fixed readability

--- src/FastStringBuilder.java
+++ src/FastStringBuilder.java
@@ -5,23 +5,25 @@ public class FastStringBuilder {
 
     public static void main(String[] args) {
         final List<String> list = new ArrayList<>();
-        for (int i = 0; i < 500_000; i++) {
+        for (int i = 0; i < 200_000; i++) {
             list.add("a");
         }
 
         String res2 = stringBuilder(list);
-        System.out.println(res2);
+        System.out.println(res2.substring(0, 5));
 
         String res1 = stringConcatenation(list);
-        System.out.println(res1);
+        System.out.println(res1.substring(0, 5));
 
         // 200_000 items
+        // Concat vs StringBuilder
         // 2800 ms - 11 ms
         // 250 times faster
 
-        // 500_000 items
-        // 14400 ms - 17 ms
-        // 850 times faster
+        // 400_000 items
+        // Concat vs StringBuilder
+        // 9000 ms - 12 ms
+        // 750 times faster
     }
 
     private static String stringConcatenation(final List<String> list) {
